c210f7e49cbc8ae0b6345353f795490cf46a9c27
Simon Mertins <mertins99@gmail.com>
2025-01-28 21:04:58 +0100

    up

--- nixos/modules/desktop.nix
+++ nixos/modules/desktop.nix
@@ -5,7 +5,7 @@
   services.xserver.enable = false;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
-  services.xserver.desktopManager.gnome.enable = false;
+  services.xserver.desktopManager.gnome.enable = true;
   programs.hyprland.enable = true;
 
     gnome = {
