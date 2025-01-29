e811b2facd33f6d9df424ed7b077c58e2d0b6a13
Simon Mertins <mertins99@gmail.com>
2025-01-29 15:52:05 +0100

    branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "My system flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
