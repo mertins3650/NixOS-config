b0a93cc5f130af75bde6ed9966a1068d6be3d001
Simon Mertins <mertins99@gmail.com>
2025-01-29 16:00:50 +0100

    font

--- nixos/modules/desktop.nix
+++ nixos/modules/desktop.nix
@@ -5,9 +5,12 @@
   services.xserver.displayManager.gdm.enable = true;
 
 
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "CascadiaCode" "CascadiaMono" ]; })
-  ];
+fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+    nerd-fonts.droid-sans-mono
+    nerd-fonts.cascadia-code
+    nerd-fonts.cascadia-mono
+];
 
   environment.systemPackages = with pkgs; [
     fuzzel
