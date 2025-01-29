eb56255b2a3f0573a28bb5f5830d036959b1db91
Simon Mertins <mertins99@gmail.com>
2025-01-29 20:14:10 +0100

    ojiasd

--- nixos/home.nix
+++ nixos/home.nix
@@ -54,7 +54,10 @@ programs.zoxide = {
     userEmail = "mertins99@gmail.com";
   };
 
-  home.file = { ".config/nvim" = { source = config.lib.file.mkOutOfStoreSymlink "/home/simonm/NixOS-config/nixos/dotfiles/config/nvim"};
+  home.file = {
+    ".config/nvim" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/simonm/NixOS-config/nixos/dotfiles/config/nvim";
+    };
   };
 
 
