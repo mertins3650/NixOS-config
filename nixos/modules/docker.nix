97866c1be3f203943e56c32b01de69ccae873f19
Simon Mertins <mertins99@gmail.com>
2025-01-29 15:21:42 +0100

    com

--- nixos/modules/docker.nix
+++ nixos/modules/docker.nix
@@ -27,19 +27,15 @@
     after = [ "docker.service" ];
     wants = [ "docker.service" ];
     serviceConfig = {
-      ExecStart = ''
-        ${pkgs.docker}/bin/docker run --name mysql8 \
-          -e MYSQL_ROOT_PASSWORD= \
-          -e MYSQL_ALLOW_EMPTY_PASSWORD=true \
-          -p "127.0.0.1:3306:3306" \
-          --restart unless-stopped \
-          -d mysql:8.4
-      '';
-      ExecStop = ''
-        ${pkgs.docker}/bin/docker stop mysql8
-        ${pkgs.docker}/bin/docker rm mysql8
-      '';
-      Restart = "always";
+      ExecStartPre = "${pkgs.docker}/bin/docker ps -a --format '{{.Names}}' | grep -q '^mysql8$' || ${pkgs.docker}/bin/docker run --name mysql8 \
+        -e MYSQL_ROOT_PASSWORD= \
+        -e MYSQL_ALLOW_EMPTY_PASSWORD=true \
+        -p '127.0.0.1:3306:3306' \
+        --restart unless-stopped \
+        -d mysql:8.4";
+      ExecStart = "${pkgs.docker}/bin/docker start mysql8";
+      ExecStop = "${pkgs.docker}/bin/docker stop mysql8";
+      Restart = "on-failure"; # Only restart if it fails, not if removed
     };
     wantedBy = [ "multi-user.target" ];
   };
@@ -49,20 +45,17 @@
     after = [ "docker.service" ];
     wants = [ "docker.service" ];
     serviceConfig = {
-      ExecStart = ''
-        ${pkgs.docker}/bin/docker run --name postgres16 \
-          -e POSTGRES_HOST_AUTH_METHOD=trust \
-          -p "127.0.0.1:5432:5432" \
-          --restart unless-stopped \
-          -d postgres:16
-      '';
-      ExecStop = ''
-        ${pkgs.docker}/bin/docker stop postgres16
-        ${pkgs.docker}/bin/docker rm postgres16
-      '';
-      Restart = "always";
+      ExecStartPre = "${pkgs.docker}/bin/docker ps -a --format '{{.Names}}' | grep -q '^postgres16$' || ${pkgs.docker}/bin/docker run --name postgres16 \
+        -e POSTGRES_HOST_AUTH_METHOD=trust \
+        -p '127.0.0.1:5432:5432' \
+        --restart unless-stopped \
+        -d postgres:16";
+      ExecStart = "${pkgs.docker}/bin/docker start postgres16";
+      ExecStop = "${pkgs.docker}/bin/docker stop postgres16";
+      Restart = "on-failure";
     };
     wantedBy = [ "multi-user.target" ];
   };
+
 }
 
