53ae3fdb30660f8ccfebe48c499eb3b87ddab6e7
Simon Mertins <mertins99@gmail.com>
2025-02-19 13:21:43 +0100

    commit

--- dotfiles/nvim/lua/mertins/lazy/colors.lua
+++ dotfiles/nvim/lua/mertins/lazy/colors.lua
@@ -1,28 +1,42 @@
 function ColorMyPencils(color)
-	color = color or "catppuccin-frappe"
-	vim.cmd.colorscheme(color)
-
-	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  --color = color or "catppuccin-mocha"
+  color = color or "rose-pine-moon"
+  vim.cmd.colorscheme(color)
 end
 
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				transparent_background = true,
-				no_italic = false,
-				styles = {
-					conditionals = {},
-					miscs = {},
-					keywords = {},
-				},
-			})
-			ColorMyPencils()
-		end,
-	},
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+        no_italic = false,
+        styles = {
+          conditionals = {},
+          miscs = {},
+          keywords = {},
+        },
+      })
+      ColorMyPencils()
+    end,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("rose-pine").setup({
+        transparent_background = true,
+        styles = {
+          italic = false,
+          transparency = true,
+        },
+      })
+      ColorMyPencils()
+    end,
+  },
 }
