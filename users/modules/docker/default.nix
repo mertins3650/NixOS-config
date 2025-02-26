5e17636b952b89e90ed8f27713c8ea7c6e80f7dd
Simon Mertins <mertins99@gmail.com>
2025-02-26 14:09:52 +0100

    docker2

--- users/modules/docker/default.nix
+++ users/modules/docker/default.nix
@@ -1,10 +1,16 @@
-{   lib, ... }:
+{ lib, ... }:
 
 {
-  home.activation.createSymlink = lib.hm.dag.entryAfter [ "dockerstart" ] ''
-    sudo docker run -d --restart unless-stopped -p "127.0.0.1:3306:3306" --name=mysql8 -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:8.4
-    sudo docker run -d --restart unless-stopped -p "127.0.0.1:5432:5432" --name=postgres16 -e POSTGRES_HOST_AUTH_METHOD=trust postgres:16
-  '';
+  home.activation.startDatabases = lib.hm.dag.entryAfter [ "dockerstart" ] ''
+    if ! docker ps --format "{{.Names}}" | grep -q mysql8; then
+      docker run -d --restart unless-stopped -p "127.0.0.1:3306:3306" --name=mysql8 \
+        -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:8.4
+    fi
 
+    if ! docker ps --format "{{.Names}}" | grep -q postgres16; then
+      docker run -d --restart unless-stopped -p "127.0.0.1:5432:5432" --name=postgres16 \
+        -e POSTGRES_HOST_AUTH_METHOD=trust postgres:16
+    fi
+  '';
 }
 
