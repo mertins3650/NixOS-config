1054b794beb00ce7cf72d638809fbe05fb7abb41
Simon Mertins <mertins99@gmail.com>
2025-02-27 11:20:40 +0100

    commit

--- modules/languages/default.nix
+++ modules/languages/default.nix
@@ -1,16 +1,11 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
 
   environment.systemPackages = with pkgs; [
-  bun
   go
-  gopls
-  nodejs_23
   python3
   rustc
-  rust-analyzer
   typescript
-  typescript-language-server
   zig
   ];
 }
