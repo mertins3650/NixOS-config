0e6d57850e78f2333192d34ea491695f4a86bc5a
Simon Mertins <mertins99@gmail.com>
2025-02-28 14:08:46 +0100

    commit

--- dotfiles/nvim/lua/mertins/lazy/lsp.lua
+++ dotfiles/nvim/lua/mertins/lazy/lsp.lua
@@ -1,3 +1,4 @@
+-- test
 return {
   {
     "folke/lazydev.nvim",
--- users/simonm.nix
+++ users/simonm.nix
@@ -77,8 +77,6 @@ in
     mkdir -p ~/.config/hypr ~/.config/nvim
     ln -s /home/simonm/NixOS-config/dotfiles/hypr/* ~/.config/hypr/
     ln -s /home/simonm/NixOS-config/dotfiles/nvim/* ~/.config/nvim/
-    sudo docker run -d --restart unless-stopped -p "127.0.0.1:3306:3306" --name=mysql8 -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:8.4
-    sudo docker run -d --restart unless-stopped -p "127.0.0.1:5432:5432" --name=postgres16 -e POSTGRES_HOST_AUTH_METHOD=trust postgres:16
   '';
 
 
