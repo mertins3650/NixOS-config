97cc7ddf82f3a271a6e93c5c58974705c065eca7
Simon Mertins <mertins99@gmail.com>
2025-03-07 16:19:01 +0100

    rm hypr

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -2,10 +2,8 @@
 
   services.xserver.enable = false;
   services.xserver.displayManager.gdm.enable = true;
-  programs.hyprland.enable = true;
   services.xserver.desktopManager.gnome.enable = false;
   services.gnome.gnome-keyring.enable = true;
-  security.pam.services.hyprland.enableGnomeKeyring = true;
   security.pam.services.sway.enableGnomeKeyring = true;
   security.pam.services.gdm.enableGnomeKeyring = true;
   security.pam.services.gdm-password.enableGnomeKeyring = true;
@@ -24,10 +22,6 @@
 
   environment.systemPackages = with pkgs; [
     nautilus
-    hypridle
-    hyprlock
-    hyprpaper
-    hyprshot
     grim
     slurp
     swayidle
